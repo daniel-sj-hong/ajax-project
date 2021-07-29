f0da433339bd5b82c6c7b94b9e3ed9746d5bebb4
daniel-sj-hong <dan8492@gmail.com>
2021-07-29 10:16:32 -0700

    added in more jokes button and edited the button with css. I just queried the button and starting to add functionality to the button

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ var $jokeContainer = document.querySelector('.joke-container');
 var $punchButton = document.querySelector('.punchline-button');
 var $ajaxTarget = document.querySelector('.ajax-target');
 var $targetColumn = document.querySelector('.target-column');
+var $moreJokesButton = document.querySelector('.more-jokes-button');
 
 $jokeButton.addEventListener('click', handleJoke);
 $punchButton.addEventListener('click', handlePunch);
+$moreJokesButton.addEventListener('click', handleMoreJokes);
 
 function getJoke() {
   var xhr = new XMLHttpRequest();
@@ -26,7 +28,7 @@ function getJoke() {
 }
 
 function handleJoke(event) {
-  $jokeContainer.className = 'joke-container';
+  $jokeContainer.className = 'joke-container margin-top-30';
   $targetColumn.className = 'hidden';
   $punchButton.className = 'punchline-button';
   getJoke();
